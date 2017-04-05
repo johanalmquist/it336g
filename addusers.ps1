1b7a7097b2cc6ff8e48a3ad789b2b72f42fe5fa9
johanalmquist <johan.almquist@danderydsitsupport.se>
2017-04-05 19:16:37 +0200

    Update addusers.ps1

--- addusers.ps1
+++ addusers.ps1
@@ -1,5 +1,7 @@
 $users =Import-CSV C:\Users\Administrator\Documents\users.csv
 
 foreach ($user in $users) {
- New-ADUser -Name $user.name -SamAccountName $user.login -Department $user.Department -EmailAddress $user.Email -Description $user.Description 
-}
+$path = "ou=$($user.Department),dc=a16johal,dc=local"
+$password = "Syp9393" | ConvertTo-SecureString -AsPlainText -Force
+ New-ADUser -Name $user.name -SamAccountName $user.login -Department $user.Department -EmailAddress $user.Email -Description $user.Description -Path $path -AccountPassword $password -Enabled $true -PasswordNeverExpires $true 
+ }
