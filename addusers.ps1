a1cfab450e58f57850e1f5d66c20ce46900cd589
johanalmquist <johan.almquist@danderydsitsupport.se>
2017-05-17 09:42:29 +0200

    Update addusers.ps1

--- addusers.ps1
+++ addusers.ps1
@@ -1,7 +1,11 @@
+# Import csv file that hold the new users
 $users =Import-CSV C:\Users\Administrator\Documents\users.csv
-
+#Add users to AD
 foreach ($user in $users) {
+#Set users path by with deperat from the csv file
 $path = "ou=$($user.Department),dc=a16johal,dc=local"
+#Set users password and convert it to a secure string
 $password = "Syp9393" | ConvertTo-SecureString -AsPlainText -Force
- New-ADUser -Name $user.name -SamAccountName $user.login -Department $user.Department -EmailAddress $user.Email -Description $user.Description -Path $path -AccountPassword $password -Enabled $true -PasswordNeverExpires $true 
+#Add the new user
+New-ADUser -Name $user.name -SamAccountName $user.login -Department $user.Department -EmailAddress $user.Email -Description $user.Description -Path $path -AccountPassword $password -Enabled $true -PasswordNeverExpires $true 
  }
